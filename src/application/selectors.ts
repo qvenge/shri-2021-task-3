916232285eb4754acece478b1a1997cde0180c8f
qvenge <coovenbm@gmail.com>
2021-04-05 19:01:49 +0700

    add return types to selectors

--- src/application/selectors.ts
+++ src/application/selectors.ts
@@ -1,28 +1,28 @@
 import { Observable } from 'rxjs';
 import { distinctUntilChanged, map } from 'rxjs/operators';
-import { DELAY, Slide, State } from './types';
+import { DELAY, Slide, State, SlideTheme } from './types';
 
 interface SlideData<T> {
     index: number;
     value: T;
 }
 
-export const createProgressSelector = (state$: Observable<State>) => state$.pipe(
+export const createProgressSelector = (state$: Observable<State>): Observable<SlideData<number>>  => state$.pipe(
     map(({ index, progress }): SlideData<number> => ({ index, value: progress / DELAY })),
     distinctUntilChanged(),    
 );
 
-export const createCurrentIndexSelector = (state$: Observable<State>) => state$.pipe(
+export const createCurrentIndexSelector = (state$: Observable<State>): Observable<number> => state$.pipe(
     map(s => s.index),
     distinctUntilChanged(),
 );
 
-export const createThemeSelector = (state$: Observable<State>) => state$.pipe(
+export const createThemeSelector = (state$: Observable<State>): Observable<SlideTheme> => state$.pipe(
     map(s => s.theme),
     distinctUntilChanged(),
 );
 
-export const createCurrentDataSelector = (state$: Observable<State>) => state$.pipe(
+export const createCurrentDataSelector = (state$: Observable<State>): Observable<SlideData<Slide>> => state$.pipe(
     map(({ index, stories }): SlideData<Slide> => ({ index, value: stories[index] })),
     distinctUntilChanged(
         (a, b) => a.index === b.index && a.value === b.value
